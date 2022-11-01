6fbf0eb2ddbfb792bf47e7cdb0ed2cf4b2b9dee4
ViktorRakhmatulin <viktor.rakhmatulin@skoltech.ru>
2022-11-01 16:01:19 +0300

    tests_with baska

--- annotation_utils.py
+++ annotation_utils.py
@@ -32,15 +32,6 @@ def detect_apriltag(img,camera_params):
         return
     return results
 
-
-def get_projection(c_to, c_from):
-    """
-    returns projection matrix by points provided
-    """
-    tform = ProjectiveTransform()
-    tform.estimate(c_to, c_from)
-    return tform
-
 def get_straight_corners(res):
     """
     normalizes corners to form a square, returns new corners' coords
@@ -57,48 +48,6 @@ def get_straight_corners(res):
     
     desired_corners = np.array([[a, b], [a, d], [c, b], [c, d]], dtype=int)
     return desired_corners
-
-def get_keypoints_on_image(img, corrected_normalized_keypoints,camera_params):
-    """
-    detects keypoints on image, with image warp and all the stuff, 
-    returns points' coords on original image 
-    """
-    res = detect_apriltag(img,camera_params)
-    img_warped, desired_corners,tform = warp_img(img, res)
-    res_warp = detect_apriltag(img_warped,camera_params)
-    warped_keypoints = get_keypoints_on_warped_image(res_warp, corrected_normalized_keypoints)
-    desired_corners = get_straight_corners(res_warp)
-#     print("desired_corners", desired_corners)
-    str_project = get_projection(desired_corners, res[0].corners)
-    print(str_project.params)
-    points_on_image = [str_project.params @ np.array([p[0], p[1], 1]) for p in warped_keypoints]
-    for p in points_on_image:
-        p/= p[2]
-    
-    return np.array(points_on_image)
- 
-def warp_img(image, res):
-    """warps image so marker would be square"""
-    desired_corners = get_straight_corners(res)
-
-    tform = get_projection(desired_corners, res[0].corners)
-    image_warped = warp(image, tform)
-    return image_warped, desired_corners, tform
-
-def draw_corners(img, r, show=False):
-    """draws marker corners, returns them"""
-    if isinstance(r, np.ndarray):
-        imagePoints = [r]
-    else:
-        imagePoints = r.corners.reshape(1,4,2)
-    
-    for corner in range(np.size(imagePoints[0],axis=0)):
-        center = ((int(imagePoints[0][corner][0]),int(imagePoints[0][corner][1])))
-        cv2.circle(img, center, 3, (0,0,255), -1)
-    
-    if show:
-        cv2.imshow('img', color_image)
-    return imagePoints[0]
 
 def get_points_from_CAD():
     # we consider that marker has 40 mm length
@@ -187,60 +136,12 @@ def draw_points_on_aruco(img, res, keypoints, show=False):
         cv2.waitKey(0)
     return scaled_keypoints
 
-def get_corrected_normalized_keypoints(res_warp,
-                                        anchors_points_normalized,
-                                        anchor_ground_truth,
-                                        normalized_keypoints):
-    
+# def apply_wart_tf(warp_tf,kpts):
 
-    def get_corrective_tf(anchor_marker, anchor_gt):
-        corrective_tf = cv2.estimateAffine2D(anchor_marker, anchor_gt,ransacReprojThreshold = 2)
-        return corrective_tf
-    
-    anchors_points = get_keypoints_on_warped_image(res_warp, anchors_points_normalized)
-    keypoints = get_keypoints_on_warped_image(res_warp, normalized_keypoints)
-    # print(keypoints)
-    corrective_tf, inliers = get_corrective_tf(anchors_points,anchor_ground_truth)
-    
-#     tform = ProjectiveTransform()
-    tform = transform.estimate_transform('euclidean', anchor_ground_truth, anchors_points)
-#     tform.estimate('euclidean',anchors_points,anchor_ground_truth)
-    print("tform.params")
-    print(tform.params)
-    print("resulting transform to correct points")
-    print(corrective_tf)
-    print("inliers", inliers)
-    dx = (res_warp[0].corners[2] - res_warp[0].corners[1])
-    center = (res_warp[0].corners[1] + res_warp[0].corners[3])/2
-    scale = dx[0]
-    homog_keypoints = np.hstack((keypoints,np.ones((keypoints.shape[0],1))))
-    # corrective_tf = tform.params
-    corrected_points =(corrective_tf @ homog_keypoints.T).T
-    print(corrected_points)
-    # print((corrective_tf @ homog_keypoints.T).T)
-    transformed_anchors_points = (corrective_tf @  np.hstack((anchors_points,np.ones((anchors_points.shape[0],1)))).T).T
-#     for p in corrected_points:
-# #         print(p)
-#         p/=p[2]
-    
-    corrected_normalized_kpts =  (corrected_points - center)/ scale
-
-    return corrected_points, corrected_normalized_kpts,transformed_anchors_points
-
-
-def get_keypoints_on_warped_image(res_warp, normalized_kpts):
-    dx = (res_warp[0].corners[2] - res_warp[0].corners[1])
-    center = (res_warp[0].corners[1] + res_warp[0].corners[3])/2
-    scale = dx[0] 
-    keypoints = normalized_kpts * scale + center
-    
-    return keypoints
-
-
-def show_save_image_with_points(img, points, comment):
+def show_image_with_points(img, points, comment):
     for c in points:
         kc = int(c[0]), int(c[1])
         cv2.circle(img, kc, 3, (0,255, 255), -1)
     cv2.imshow(comment, img)
     cv2.waitKey(0)
-    cv2.imwrite(comment+".jpg", img)
+    # cv2.imwrite(comment+".jpg", img)
